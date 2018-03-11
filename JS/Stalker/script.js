12b259612cadda44bf957003e698057b618c41a4
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-11 19:46:51 +0800

    Update age calculator JS exercise

--- JS/Stalker/script.js
+++ JS/Stalker/script.js
@@ -1,3 +1,4 @@
+// Stalker
 // var firstName = prompt("What is your first name?");
 
 // var lastName = prompt("What is your last name?");
@@ -7,6 +8,19 @@
 // console.log("Your name is " + firstName + " " + lastName + ".");
 // console.log("You are " + age + " year(s) old.");
 
+// Age Calculator
+// var age = prompt("How old are you?");
+// var daysAlive = age * 365.25;
+// console.log("You have been alive for at least " + daysAlive + " days.");
+
+// Conditionals
 var age = prompt("How old are you?");
-var daysAlive = age * 365.25;
-console.log("You have been alive for at least " + daysAlive + " days.");
+if (age < 0) {
+    console.log("ERROR!");
+} else if (age == 21) {
+    console.log("Happy 21st birthday!");
+} else if (age % 2 != 0) {
+    console.log("Your age is odd!");
+} else if (age % Math.sqrt(age) == 0) {
+    console.log("perfect square!");
+}

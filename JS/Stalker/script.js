c0f826a9aa197dfaad74f71c045870ec98c0842d
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-11 19:25:39 +0800

    Add basic JS exercises

---
+++ JS/Stalker/script.js
@@ -0,0 +1,12 @@
+// var firstName = prompt("What is your first name?");
+
+// var lastName = prompt("What is your last name?");
+
+// var age = prompt("What is your age?");
+
+// console.log("Your name is " + firstName + " " + lastName + ".");
+// console.log("You are " + age + " year(s) old.");
+
+var age = prompt("How old are you?");
+var daysAlive = age * 365.25;
+console.log("You have been alive for at least " + daysAlive + " days.");

2fc5287bbb2a91634a070b0b92b73c71b0e13d3e
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-14 20:28:22 +0800

    Add jQuery project files

---
+++ jQuery/Effects/script.js
@@ -0,0 +1,10 @@
+// $("button").on("click", function() {
+//     $("div").fadeOut(1000, function() {
+//         // actually remove the elements
+//         $(this).remove();
+//     });
+// });
+
+$("button").on("click", function() {
+    $("div").slideToggle();
+});
